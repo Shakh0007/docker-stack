af1352e06a18efbefb89754e330319ceb9f33363
shakh <shakhri707@gmail.com>
2022-03-17 05:31:04 +0000

    modified swarm and stack

--- swarm.sh
+++ swarm.sh
@@ -6,7 +6,7 @@ sudo systemctl enable docker
 systemctl status docker 
 
 
-docker swarm join --token SWMTKN-1-4okiq3uwqroe1o9mbx1rr9fpotxenmu2wivgoxho6nnfalwg7g-4orjw2pncewmne7envjwsc9p8 159.65.182.125:2377
+docker swarm join --token SWMTKN-1-030mfkwm2fpdgx78p6llle7k587e9n952mhozocpnznj6y4r1z-55gph5mt8u70qean9bxfio8d5 161.35.139.51:2377
 
 
 docker service create    \
